5288a4e7234629be893c2872b7481d9ffd05d879
brightkut <dsorn2@gmail.com>
2025-03-01 19:09:06 +0700

    feat: implement view wallet command

--- src/main/java/com/brightkut/walley_v2/command/wallet/ViewWalletCommand.java
+++ src/main/java/com/brightkut/walley_v2/command/wallet/ViewWalletCommand.java
@@ -4,13 +4,20 @@ import org.springframework.stereotype.Component;
 
 import com.brightkut.walley_v2.command.BaseCommand;
 import com.brightkut.walley_v2.constant.CommonConstant;
+import com.brightkut.walley_v2.service.WalletService;
 
 @Component
 public class ViewWalletCommand implements BaseCommand{
 
+    private final WalletService walletService;
+
+    public ViewWalletCommand(WalletService walletService){
+        this.walletService = walletService;
+    }
+    
     @Override
     public String command(String msg, String userId) {
-        return "จำนวณเงิน 0 บาท";
+        return walletService.getWallet(userId);
     }
 
     @Override
--- src/main/java/com/brightkut/walley_v2/constant/CommonConstant.java
+++ src/main/java/com/brightkut/walley_v2/constant/CommonConstant.java
@@ -7,6 +7,9 @@ public class CommonConstant {
     public static final String SUMMARIZE_WALLET = "สรุปผลการเงิน";
     public static final String UPDATE_WALLET_BY_BILL = "ต้องการ Upload รูปภาพบิลค่าใช้จ่าย";
 
+    public static final String CREATE_WALLET_RES = "สร้างกระเป๋าเงินใหม่ และมีจำนวณเงินในกระเป๋า %s บาท";
+    public static final String VIEW_WALLET_RES = "มีจำนวณเงินในกระเป๋า %s บาท";
+
     // Status
     public static String ERROR_COMMAND_NOT_FOUND = "ไม่พบข้อมูลของคำสั่งนี้ในระบบ";
     public static String SUCCESS_STATUS_FROM_LINE = "Success";
